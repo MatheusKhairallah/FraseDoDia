26418745d58fcd30c82eb2b6be9a7a4086de47fe
Matheus <playermaster10like@gmail.com>
2020-10-19 10:39:56 -0300

    segundo teste API

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const exibeFrase = (quote) => {
+const exibeFrase= (quote)=> {
     const texto= document.getElementById("texto");
     texto.innerHTML= quote.quoteText;
     const autor= document.getElementById("autor");
     autor.innerHTML= quote.quoteAuthor;
 };
 
+const urlFrasealeatoria= ()=> {
+    const urlFrase= 'https://quote-garden.herokuapp.com/api/v2/quotes/random';
+    return urlFrase;
+};
+
 const iniciar = async () => {
-    const response = await fetch('https://quote-garden.herokuapp.com/api/v2/quotes/random');
+    const url= urlFrasealeatoria();
+    const response = await fetch(url);
     const result = await response.json();
     exibeFrase(result);
 };
